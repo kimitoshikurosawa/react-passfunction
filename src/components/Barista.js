4840c163a24fc7dcd7f30bb73ccce419bd7eea33
Kim <misagokim@gmail.com>
2022-08-07 20:49:02 +0000

    finish little project CoffeShop

--- src/components/Barista.js
+++ src/components/Barista.js
@@ -4,12 +4,13 @@ import baristaimg from "./barista.jpg";
 
 
 let Barista = props => {
-    const btnAnswer = props.theState.clientMessage !== null ? (<button>Answser</button>): (<button disabled> Answer</button>) ;
+    const btnAnswer = props.theState.clientMessage !== null ? (<button onClick={props.orderAnswer}>Answer</button>): (<button disabled> Answer</button>) ;
     return(
         <div className="Bartender">
             <img width="150px" height="100px" src={baristaimg} alt="" className="src"/>
-            <h2>I'm {props.name} the bartender</h2>
+            <h2>I'm {props.name} the Barista</h2>
             {btnAnswer}
+            <p>{props.theState.baristaMessage}</p>
         </div>
     )
 }
--- src/components/Client.js
+++ src/components/Client.js
@@ -5,24 +5,29 @@ import Barista from "./Barista";
 class Client extends Component {
     state = {
         clientMessage: null,
-        bartenderMessage: null
+        baristaMessage: null
     }
     orderCoffee = () => {
         this.setState({
             clientMessage: 'I want a Double Nespresso'
         })
+    }
 
+    baristaAnswer = () => {
+        this.setState({
+            baristaMessage: 'Ok! we have take your order'
+        })
     }
 
     render() {
         return (
             <div>
-                <img  src={clientimg} alt="" className="src"/>
+                <img width="250px" height="200px" src={clientimg} alt="" className="src"/>
                 <h1>Client</h1>
                 <button onClick={this.orderCoffee}> Order Coffee </button>
                 <p>{this.state.clientMessage}</p>
                 <hr/>
-                <Barista name="Jack" theState={this.state}/>
+                <Barista name="Jack" orderAnswer={this.baristaAnswer} theState={this.state}/>
             </div>
         )
 
